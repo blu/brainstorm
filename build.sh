34cd8d347c4028eed2824ec78a20b915dc953e0c
Teodor Petrov <fuscated@gmail.com>
2023-06-22 01:09:49 +0300

    Print the compiler version

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 CXX=${CXX:-g++}
 
 # echo "Using CXX=$CXX"
+${CXX} --version
 
 ${CXX} -Ofast -fstrict-aliasing -fno-rtti -fno-exceptions -march=native -mtune=native main${1}.cpp util_file.cpp -o brinterp -DNDEBUG -DPRINT_ASCII=1 -DENABLE_DIAGNOSTICS=0
