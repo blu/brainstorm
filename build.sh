770f4d6d3c354796fa63e27aad499568899a971f
Martin Krastev <blu.dark@gmail.com>
2025-09-23 16:21:05 -0400

    add uarch survey on linux/aarch64 for compiler tuning

--- build.sh
+++ build.sh
@@ -1,7 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 CXX=${CXX:-g++}
-
 # echo "Using CXX=$CXX"
 
-${CXX} -Ofast -fstrict-aliasing -fno-rtti -fno-exceptions -march=native -mtune=native main${1}.cpp util_file.cpp -o brinterp -DNDEBUG -DPRINT_ASCII=1 -DENABLE_DIAGNOSTICS=0
+source cxx_util.sh
+
+UNAME_MACHINE=`uname -m`
+CXXFLAGS=(
+	-Ofast
+	-fstrict-aliasing
+	-fno-rtti
+	-fno-exceptions
+	-DNDEBUG
+	-DPRINT_ASCII=1
+	-DENABLE_DIAGNOSTICS=0
+)
+if [[ $UNAME_MACHINE == "aarch64" ]] ; then
+
+		cxx_uarch_arm
+
+elif [[ $UNAME_MACHINE == "arm64" ]] ; then
+		CXXFLAGS+=(
+			-march=armv8.4-a # baseline for macos/arm64
+			-mtune=native
+		)
+else
+		CXXFLAGS+=(
+			-march=native
+			-mtune=native
+		)
+fi
+
+# set -x
+${CXX} ${CXXFLAGS[@]} main${1}.cpp util_file.cpp -o brinterp
